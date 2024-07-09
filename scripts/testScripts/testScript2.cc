a70f0c37d3a75b01bf2ff90674e0214fd6cbef50
Nova Travis <glowstar5297@gmail.com>
2024-07-09 12:50:19 -0400

    Test Script 2 (ggh att2)

--- scripts/testScripts/testScript2.cc
+++ scripts/testScripts/testScript2.cc
@@ -8,10 +8,12 @@ int main(){
     //First Attempt at 25 event gluon fusion channel (ggH)
     int nEvents = 25;
     Pythia8::Pythia pythia;
-    pythia.readString("Beams:idA = 21");
-    pythia.readString("Beams:idB = 21");
+
+    pythia.readString("Beams:idA = 2212");
+    pythia.readString("Beams:idB = 2212");
+    
     pythia.readString("Beams:eCM = 13.e3");
-    pythia.readString("SoftQCD:all = on");
+
     pythia.readString("HardQCD:all = on");
     pythia.readString("HiggsSM:gg2H = on");
     
@@ -27,16 +29,13 @@ int main(){
         std::cout << "Event size: " << entries << std::endl;
 
         //Collects data from each entry in the event
-        for(int j = 0; j < entries; j++){
+        for(int j = 0; j < entries; j++) {
             int id = pythia.event[j].id();
-
             double mass = pythia.event[j].m();
-
             double px = pythia.event[j].px();
             double py = pythia.event[j].py();
             double pz = pythia.event[j].pz();
             double pabs = sqrt(pow(px,2) + pow(py,2) + pow(pz,2));
-
             double eta = pythia.event[j].eta();
 
             std::cout << "Entry: " << id << " > Mass: " << mass << " > Momentum: " << pabs << std::endl;
