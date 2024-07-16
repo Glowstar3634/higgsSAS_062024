d0a0ef34d47e07d2310965f49475d8bbbd2f0dbf
Nova Travis <glowstar5297@gmail.com>
2024-07-15 20:17:46 -0400

    ggH ParticleGen - att3

--- scripts/unityScripts/ggHparticleGen.cpp
+++ scripts/unityScripts/ggHparticleGen.cpp
@@ -3,44 +3,52 @@
 #include <fstream>
 #include "Pythia8/Pythia.h"
 
-extern "C" __declspec(dllexport) void GenerateEvents() {
-    void generateParticleData(const char* outputFilePath) {
-        using namespace Pythia8;
-        int nEvents = 25;
-        Pythia pythia;
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__ ((visibility ("default")))
+#endif
 
-        pythia.readString("Random:setSeed = on");
-        pythia.readString("Random:seed = " + std::to_string(time(0)));
-        pythia.readString("Beams:idA = 2212");
-        pythia.readString("Beams:idB = 2212");
-        pythia.readString("Beams:eCM = 13.e3");
-        pythia.readString("HardQCD:all = on");
-        pythia.readString("HiggsSM:gg2H = on");
-        
-        pythia.init();
+void generateParticleData(const char* outputFilePath) {
+    using namespace Pythia8;
+    int nEvents = 25;
+    Pythia pythia;
 
-        std::ofstream outFile(outputFilePath);
-        outFile << "event_id,particle_id,mass,px,py,pz,eta" << std::endl;
+    pythia.readString("Random:setSeed = on");
+    pythia.readString("Random:seed = " + std::to_string(time(0)));
+    pythia.readString("Beams:idA = 2212");
+    pythia.readString("Beams:idB = 2212");
+    pythia.readString("Beams:eCM = 13.e3");
+    pythia.readString("HardQCD:all = on");
+    pythia.readString("HiggsSM:gg2H = on");
+    
+    pythia.init();
 
-        for (int i = 0; i < nEvents; i++) {
-            if (!pythia.next()) { continue; }
+    std::ofstream outFile(outputFilePath);
+    outFile << "event_id,particle_id,mass,px,py,pz,eta" << std::endl;
 
-            int entries = pythia.event.size();
+    for (int i = 0; i < nEvents; i++) {
+        if (!pythia.next()) { continue; }
 
-            for (int j = 0; j < entries; j++) {
-                int id = pythia.event[j].id();
-                if (id == 25 || id == 35 || id == 36 || id == 37) {
-                    double mass = pythia.event[j].m();
-                    double px = pythia.event[j].px();
-                    double py = pythia.event[j].py();
-                    double pz = pythia.event[j].pz();
-                    double eta = pythia.event[j].eta();
+        int entries = pythia.event.size();
 
-                    outFile << i << "," << id << "," << mass << "," << px << "," << py << "," << pz << "," << eta << std::endl;
-                }
+        for (int j = 0; j < entries; j++) {
+            int id = pythia.event[j].id();
+            if (id == 25 || id == 35 || id == 36 || id == 37) {
+                double mass = pythia.event[j].m();
+                double px = pythia.event[j].px();
+                double py = pythia.event[j].py();
+                double pz = pythia.event[j].pz();
+                double eta = pythia.event[j].eta();
+
+                outFile << i << "," << id << "," << mass << "," << px << "," << py << "," << pz << "," << eta << std::endl;
             }
         }
-
-        outFile.close();
     }
+
+    outFile.close();
+}
+
+extern "C" EXPORT void GenerateEvents(const char* outputFilePath) {
+    generateParticleData(outputFilePath);
 }
