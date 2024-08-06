d1f7b1816418e5aae2bbf464f91463ff989e868d
Nova Travis <glowstar5297@gmail.com>
2024-08-06 01:06:08 -0400

    Test Server Script 6

--- scripts/serverScripts/server.js
+++ scripts/serverScripts/server.js
@@ -22,6 +22,6 @@ app.get('/generate', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
 });
