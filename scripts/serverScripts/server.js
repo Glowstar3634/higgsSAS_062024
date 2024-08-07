9068c67fc60aecefce234ddfde2ba861dfaafc09
Nova Travis <glowstar5297@gmail.com>
2024-08-07 02:08:51 -0400

    Test Server Script 1.09

--- scripts/serverScripts/server.js
+++ scripts/serverScripts/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/generate', (req, res) => {
-    exec('/home/azureuser/pythia8312/scripts/testScript2_07 /home/azureuser/pythia8312/scripts/particleData2_07.csv', { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+    exec('/home/azureuser/pythia8312/scripts/testScript2_07 /home/azureuser/pythia8312/scripts/particleData2_07.csv', { maxBuffer: 5 * 1024 * 1024 }, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             res.status(500).send(`Error: ${error.message}`);
