53d0e91097887b245e059aa29d3d810448bf4f47
Nova Travis <glowstar5297@gmail.com>
2024-09-09 22:32:55 -0400

    new server script 1.02

--- scripts/serverScripts/server.js
+++ scripts/serverScripts/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/generate', (req, res) => {
-    exec('/home/ubuntu/pythia8312/scripts/pgen_ggHND_01 /home/ubuntu/pythia8312/scripts/particleData4_01.csv', { maxBuffer: 3 * 1024 * 1024 }, (error, stdout, stderr) => {
+    exec('/home/ubuntu/pythia8312/scripts/pgen1.02ggHND /home/ubuntu/pythia8312/scripts/particleData4_01.csv', { maxBuffer: 3 * 1024 * 1024 }, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             res.status(500).send(`Error: ${error.message}`);
