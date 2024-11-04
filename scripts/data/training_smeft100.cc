3388a581a21ea7ad196affbf1f087009df4e5c20
Nova Travis <glowstar5297@gmail.com>
2024-11-04 13:48:16 -0500

    MGxP8 t3

--- scripts/data/training_smeft100.cc
+++ scripts/data/training_smeft100.cc
@@ -35,8 +35,8 @@ int main(int argc, char* argv[]) {
     pythia.readString("Random:setSeed = on");
     pythia.readString("Random:seed = 0");
     pythia.readString("Beams:frameType = 4");
-    pythia.readString("Beams:LHEF = 'path_to_lhe_file.lhe'");
-    //pythia.readString("Beams:eCM = 100.e3");
+    pythia.readString("Beams:LHEF = '/home/ubuntu/MG5_aMC_v3_6_0/SMEFT_run2/Events/run_01/unweighted_events.lhe'");
+    pythia.readString("Beams:eCM = 100.e3");
     pythia.readString("HiggsSM:all  = off");
     pythia.readString("HiggsSM:gg2H = on"); // Enable gg -> H (ggH)
     pythia.readString("HiggsSM:ff2Hff(t:ZZ) = on"); // Enable VBF (VBF: quark initiated)
@@ -64,15 +64,34 @@ int main(int argc, char* argv[]) {
 
     for (int i = 0; i < nEvents; i++) {
         if (!pythia.next()) continue;
+
+        // Identify Higgs production process in LHE-initiated event
+        int id1 = pythia.info.id1();  // PDG code of incoming particle 1
+        int id2 = pythia.info.id2();  // PDG code of incoming particle 2
+        int productionChannel = 0;
+
+        // Infer production channel from incoming partons
+        if (abs(id1) == 21 && abs(id2) == 21) {
+            productionChannel = 1;  // gg -> H (gluon fusion)
+        } else if (abs(id1) <= 6 && abs(id2) <= 6 && id1 * id2 < 0) {
+            // Check specific quark combinations for VBF (u, d, c, s)
+            if ((abs(id1) == 1 || abs(id1) == 2) && (abs(id2) == 1 || abs(id2) == 2)) {
+                productionChannel = 2;  // VBF
+            } else {
+                productionChannel = 3;  // VH (associated production)
+            }
+        } else if ((abs(id1) == 6 && abs(id2) == -6) || (abs(id1) == -6 && abs(id2) == 6)) {
+            productionChannel = 4;  // tt -> H (ttH production)
+        }
+
         for (int j = 0; j < pythia.event.size(); j++) {
             if (pythia.event[j].id() == 25 && pythia.event[j].status() == -62) {
+                // Higgs decay logic
                 totalHCount++;
 
                 // Store decay products and their momenta
                 std::vector<int> decayProducts;
                 std::vector<Vec4> momenta;
-                int productionChannel = pythia.info.code();
-
                 std::map<int, int> particleIdToIndexMap;
 
                 for (int k = 0; k < pythia.event.size(); k++) {
@@ -99,6 +118,7 @@ int main(int argc, char* argv[]) {
             }
         }
     }
+
 
     //Finished
     outFile.close();
