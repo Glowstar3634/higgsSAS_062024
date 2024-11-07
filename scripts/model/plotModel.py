7d4e48fdcdcc1308f6a5b0201e862b135931b7cb
Nova Travis <glowstar5297@gmail.com>
2024-11-07 16:51:44 -0500

    PM2

--- scripts/model/plotModel.py
+++ scripts/model/plotModel.py
@@ -1,10 +1,11 @@
 import sys
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import plot_model
 
 def main(model_path):
     # Load the model from the provided path
-    model = load_model(model_path)
+    model = load_model(model_path, custom_objects={'mse': tf.keras.losses.MeanSquaredError})
     
     plot_model(model, to_file='model_diagram.png', show_shapes=True, show_layer_names=True)
     print(f"Model diagram saved as 'model_diagram.png'.")
